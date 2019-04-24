b06451d54626e374e58f9c7670d7ca1d7ca35368
Francisco Javier Sánchez de la Torre <fjsancheztrabajos@gmail.com>
2019-04-24 02:44:49 +0200

    Test components and add login

---
+++ src/components/login.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="row justify-content-around">
+        <div class="col-sm-4">
+            <form>
+                <p class="h4 text-center mb-4">Sign in</p>
+                <div class="grey-text text-left">
+                    <mdb-input label="Email" icon="envelope" type="email"/>
+                    <mdb-input label="Contraseña" icon="lock" type="password"/>
+                </div>
+                <div class="text-center">
+                    <mdb-btn>Entrar</mdb-btn>
+                </div>
+            </form>
+        </div>
+
+        <div class="col-sm-4">
+            <template>
+                <form>
+                    <p class="h4 text-center mb-4">Sign up</p>
+                    <div class="grey-text text-left">
+                        <mdb-input label="Nombre usuario" icon="user" type="text"/>
+                        <mdb-input label="Email" icon="envelope" type="email"/>
+                        <mdb-input label="Confirma tu email" icon="exclamation-triangle" type="text"/>
+                        <mdb-input label="Contraseña" icon="lock" type="password"/>
+                    </div>
+                    <div class="text-center">
+                        <mdb-btn color="primary">Registrar</mdb-btn>
+                    </div>
+                </form>
+            </template>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    import {mdbInput, mdbBtn} from 'mdbvue';
+
+    export default {
+        name: 'login',
+        components: {
+            mdbInput,
+            mdbBtn
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
+//////////
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbvue/build/css/mdb.css';
+//TIENEN QUE ESTAR ANTES DEL APP.VUE!!!!!!!!!!
 import App from './App.vue'
+import router from './router'
+import firebase from 'firebase';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyBYMuprswO6DZ4WB9oTRAyrpH-qLDWeVSs",
+    authDomain: "fran-daw.firebaseapp.com",
+    databaseURL: "https://fran-daw.firebaseio.com",
+    projectId: "fran-daw",
+    storageBucket: "fran-daw.appspot.com",
+    messagingSenderId: "982666750970"
+};
+firebase.initializeApp(config);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    router,
+    render: h => h(App)
+}).$mount('#app');
