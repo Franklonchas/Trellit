12e79b16031a23d462f086351f447c1612c4a4bd
Fran Sánchez <fjsanchez@trevenque.es>
2019-04-24 14:31:42 +0200

    fix footer, removed hello component

--- src/components/my-footer.vue
+++ src/components/my-footer.vue
@@ -1,28 +1,31 @@
 <template>
+
     <mdb-footer color="green lighten-2" class="font-small pt-4 mt-4">
         <mdb-container class="text-left">
             <mdb-row>
                 <mdb-col sm="6">
-                    <h5 class="title">Footer Content</h5>
-                    <p>Here you can use rows and columns here to organize your footer content.</p>
+                    <h5 class="title">Trellit</h5>
+                    <p>Este es un pryecto independiente, desarrollado para ayudar y hacer uso de tecnologias
+                        innovadoras y pioneras.</p>
                 </mdb-col>
                 <mdb-col sm="6">
-                    <h5 class="title">Links</h5>
+                    <h5 class="title">Links de interés</h5>
                     <ul>
-                        <li class="list-unstyled"><a href="#!">Link 1</a></li>
-                        <li class="list-unstyled"><a href="#!">Link 2</a></li>
-                        <li class="list-unstyled"><a href="#!">Link 3</a></li>
-                        <li class="list-unstyled"><a href="#!">Link 4</a></li>
+                        <li class="list-unstyled"><a href="#!">Github del desarrollador</a></li>
+                        <li class="list-unstyled"><a href="#!">Mapa de la web</a></li>
+                        <li class="list-unstyled"><a href="#!">Información legal</a></li>
+                        <li class="list-unstyled"><a href="#!">Portfolio personal</a></li>
                     </ul>
                 </mdb-col>
             </mdb-row>
         </mdb-container>
         <div class="footer-copyright text-center py-3">
             <mdb-container fluid>
-                &copy; 2018 Copyright: <a href="https://www.MDBootstrap.com"> MDBootstrap.com </a>
+                &copy; 2019 Derechos reservados a: <a href="#"> Francisco Javier Sánchez de la Torre </a>
             </mdb-container>
         </div>
     </mdb-footer>
+
 </template>
 
 <script>
@@ -40,5 +43,7 @@
 </script>
 
 <style scoped>
-
+    .mt-4 {
+        margin-top: 0 !important;
+    }
 </style>
