26503af7f594eb49d7d900bd1f5d7a919dbd9740
Francisco Javier Sánchez de la Torre <fjsancheztrabajos@gmail.com>
2019-05-20 01:27:14 +0200

    check the messages to show in chat

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -1,5 +1,14 @@
 <template>
     <div class="winter-neva-gradient">
+        <div>
+            <div v-for="mensajes in arrayMensajes" v-bind:key="mensajes.fecha">
+                <div>
+                    Autor: {{mensajes.autor}}
+                    Contenido: {{mensajes.mensaje}}
+                    comprobacion: {{mensajes.local}}
+                </div>
+            </div>
+        </div>
         <label>
             <input v-model="localMensaje" type="text">
         </label>
@@ -15,8 +24,10 @@
         data: function () {
             return {
                 localMensaje: '',
+                localAutor: '',
+                arrayMensajes: [],
                 localUser: '',
-                arrayMensajes: []
+                loadMessage: ''
             }
         },
         methods: {
@@ -24,19 +35,34 @@
                 this.arrayMensajes = [];
 
                 for (let key in mensajes) {
-                    this.arrayMensajes.push({
-                        mensaje: mensajes[key].mensaje,
-                        autor: mensajes[key].autor,
-                        fecha: mensajes[key].fecha,
-                    })
+                    this.parseUser2();
+                    if (this.loadMessage === mensajes[key].usuario) {
+                        this.arrayMensajes.push({
+                            mensaje: mensajes[key].mensaje,
+                            autor: mensajes[key].autor,
+                            fecha: mensajes[key].fecha,
+                            usuario: mensajes[key].usuario,
+                            local: true
+                        });
+                    }
+                    if (this.loadMessage !== mensajes[key].usuario) {
+                        this.arrayMensajes.push({
+                            mensaje: mensajes[key].mensaje,
+                            autor: mensajes[key].autor,
+                            fecha: mensajes[key].fecha,
+                            usuario: mensajes[key].usuario,
+                            local: false
+                        });
+                    }
                 }
             },
             pasrseUser: function (user) {
                 for (let i = 21; i < user.length; i++) {
-                    this.localUser += user[i];
+                    this.localAutor += user[i];
                 }
-                this.localUser = this.localUser.split('@trevenque.es');
-                return this.localUser[0];
+                this.localUser = this.localAutor;
+                this.localAutor = this.localAutor.split('@trevenque.es');
+                return this.localAutor[0];
             },
             addMessage: function () {
                 let today = new Date();
@@ -44,10 +70,19 @@
                 this.pasrseUser(user);
                 firebase.database().ref('chat/').push({
                     mensaje: this.localMensaje,
-                    autor: this.localUser[0],
+                    autor: this.localAutor[0],
                     fecha: today.toLocaleDateString("es-ES") + " " + today.getHours() + ":" + today.getMinutes()
                         + ":" + today.getSeconds(),
+                    usuario: this.localUser
                 });
+            },
+            parseUser2: function () {
+                let user = localStorage.getItem('sesion_activa');
+                this.loadMessage = '';
+                for (let i = 21; i < user.length; i++) {
+                    this.loadMessage += user[i];
+                }
+                return this.loadMessage;
             }
         },
         mounted() {
