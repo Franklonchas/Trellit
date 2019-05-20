1696809c36150f657239eb3146bacb06b03756e3
Fran Sánchez <fjsanchez@trevenque.es>
2019-05-20 13:20:12 +0200

    update calendar (no events)

--- src/components/calendar.vue
+++ src/components/calendar.vue
@@ -1,28 +1,31 @@
 <template>
     <div id="cal">
-        <h1>My Calendar</h1>
+        <h1 style="text-align: center">Roadmap Global</h1>
+        <br>
         <calendar-view
                 :show-date="showDate"
+                :startingDayOfWeek="1"
                 class="theme-default holiday-us-traditional holiday-us-official">
             <calendar-view-header
                     slot="header"
                     slot-scope="t"
                     :header-props="t.headerProps"
-                    @input="setShowDate" />
+                    @input="setShowDate"/>
         </calendar-view>
         <br><br>
     </div>
 </template>
 
 <script>
-    import { CalendarView, CalendarViewHeader } from "vue-simple-calendar"
+    import {CalendarView, CalendarViewHeader} from "vue-simple-calendar"
     require("vue-simple-calendar/static/css/default.css");
     require("vue-simple-calendar/static/css/holidays-us.css");
 
     export default {
         name: "calendar",
-        data: function() {
-            return { showDate: new Date()
+        data: function () {
+            return {
+                showDate: new Date(),
             }
         },
         components: {
@@ -33,8 +36,8 @@
             setShowDate(d) {
                 this.showDate = d;
             },
-        }
-
+        },
+        props: {}
     }
 </script>
 
