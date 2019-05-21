9360b4280bd8fef38b2189b3b3d0e3ad8c586fdd
Fran Sánchez <fjsanchez@trevenque.es>
2019-05-21 12:58:45 +0200

    fix footer info and updated

--- src/components/my-footer.vue
+++ src/components/my-footer.vue
@@ -5,22 +5,31 @@
             <mdb-row>
                 <mdb-col sm="6">
                     <h5 class="title">Trellit</h5>
-                    <p>Este es un pryecto independiente, desarrollado para ayudar y hacer uso de tecnologias
-                        innovadoras y pioneras.</p>
+                    <p>Este es un proyecto independiente, desarrollado para ayudar y hacer uso de tecnologias
+                        innovadoras y pioneras.
+                        Todos los datos guardados en esta web respetan sus derechos de
+                        privacidad, para nosotros siempre
+                        es una prioridad.</p>
                 </mdb-col>
                 <mdb-col sm="6">
                     <h5 class="title">Links de interés</h5>
                     <ul>
-                        <li class="list-unstyled"><a href="#!">Github del desarrollador</a></li>
-                        <li class="list-unstyled"><a href="#!">Mapa de la web</a></li>
-                        <li class="list-unstyled"><a href="#!">Portfolio personal</a></li>
+                        <li class="list-unstyled"><a href="https://github.com/Franklonchas">
+                            Github del desarrollador&nbsp;&nbsp;&nbsp;<mdb-icon fab icon="github"/>
+                        </a></li>
+                        <li class="list-unstyled"><a href="#!">
+                            Mapa de la web&nbsp;&nbsp;&nbsp;<mdb-icon icon="map-marked-alt"/>
+                        </a></li>
+                        <li class="list-unstyled"><a href="#!">Portfolio personal&nbsp;&nbsp;&nbsp;<mdb-icon
+                                icon="user-circle"/>
+                        </a></li>
                     </ul>
                 </mdb-col>
             </mdb-row>
         </mdb-container>
         <div class="footer-copyright text-center py-3">
             <mdb-container fluid>
-                &copy; 2019 Derechos reservados a: <a href="#"> Francisco Javier Sánchez de la Torre </a>
+                &copy; 2019 Derechos reservados a: <a href="#"> Francisco Javier Sánchez de la Torre & Trellit </a>
             </mdb-container>
         </div>
     </mdb-footer>
@@ -28,7 +37,7 @@
 </template>
 
 <script>
-    import {mdbFooter, mdbContainer, mdbRow, mdbCol} from 'mdbvue';
+    import {mdbFooter, mdbContainer, mdbRow, mdbCol, mdbIcon} from 'mdbvue';
 
     export default {
         name: 'my-footer',
@@ -36,7 +45,8 @@
             mdbFooter,
             mdbContainer,
             mdbRow,
-            mdbCol
+            mdbCol,
+            mdbIcon
         }
     }
 </script>
